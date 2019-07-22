154c68383cdd737c14aa62a02e0298e9aaf3e20e
Siddharth Vashisht <31439718+Sidvashisht008@users.noreply.github.com>
2019-07-22 13:07:58 +0530

    Create .discard() remove & pop.py

---
+++ .discard() remove & pop.py
@@ -0,0 +1,12 @@
+n=int(input())
+s=set(map(int,input().split()))
+N=int(input())
+for i in range(1,N+1):
+    a=input()
+    if a=="pop":
+        s.pop()
+    elif a.split()[0]=='discard':
+        s.discard(int(a.split()[1]))
+    elif a.split()[0]=="remove":
+        s.remove(int(a.split()[1]))
+print(sum(list(s)))
